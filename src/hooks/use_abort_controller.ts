287443f65e41ee497104df3981d7af7048306064
Fabio Spampinato <spampinabio@gmail.com>
2022-02-25 19:14:58 +0000

    useAbortController: added support for using external signals signals too

--- src/hooks/use_abort_controller.ts
+++ src/hooks/use_abort_controller.ts
@@ -1,16 +1,56 @@
 
 /* IMPORT */
 
+import {$} from '../observable';
 import useCleanup from './use_cleanup';
+import useEffect from './use_effect';
 
 /* MAIN */
 
-//TODO: Support parent signals, and use it in useFetch
+const useAbortController = ( signals?: AbortSignal[] ): AbortController => {
 
-const useAbortController = (): AbortController => {
-
+  const aborted = $(false);
   const aborter = new AbortController ();
-  const abort = aborter.abort.bind ( aborter );
+
+  const abort = (): void => {
+
+    aborter.abort ();
+
+    aborted ( true );
+
+  };
+
+  if ( signals ) {
+
+    useEffect ( () => {
+
+      if ( aborted () ) return;
+
+      for ( let i = 0, l = signals.length; i < l; i++ ) {
+
+        if ( signals[i].aborted ) return abort ();
+
+      }
+
+      for ( let i = 0, l = signals.length; i < l; i++ ) {
+
+        signals[i].addEventListener ( 'abort', abort );
+
+      }
+
+      return () => {
+
+        for ( let i = 0, l = signals.length; i < l; i++ ) {
+
+          signals[i].removeEventListener ( 'abort', abort );
+
+        }
+
+      };
+
+    });
+
+  }
 
   useCleanup ( abort );
 
