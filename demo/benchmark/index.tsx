c344d0df5c13e794a65897208523b7e456f57c4c
Fabio Spampinato <spampinabio@gmail.com>
2022-02-25 21:54:56 +0000

    Updated benchmark demo

--- demo/benchmark/index.tsx
+++ demo/benchmark/index.tsx
@@ -1,15 +1,16 @@
 
 /* IMPORT */
 
-import type {Observable, ObservableMaybe} from 'voby/dist/types';
-import {For} from 'voby';
-import {$, render, template} from 'voby';
+import {Observable, ObservableMaybe} from 'voby';
+import {$, render, template, For} from 'voby';
 
-/* MAIN */
+/* TPYES */
 
 type IDatum = Observable<{ id: string, label: Observable<string>, selected: Observable<boolean>, className: Observable<string> }>;
 
 type IData = IDatum[];
+
+/* HELPERS */
 
 const rand = ( max: number ): number => {
   return Math.round ( Math.random () * 1000 ) % max;
@@ -17,7 +18,9 @@ const rand = ( max: number ): number => {
 
 const uuid = (() => {
   let counter = 0;
-  return () => String ( counter++ );
+  return (): string => {
+    return String ( counter++ );
+  };
 })();
 
 const buildData = (() => {
@@ -41,12 +44,14 @@ const buildData = (() => {
   };
 })();
 
+/* MODEL */
+
 const Model = (() => {
 
   /* STATE */
 
-  let data$ = $<IDatum[]>( [] );
-  let selected$: IDatum | null = null;
+  let $data = $<IDatum[]>( [] );
+  let $selected: IDatum | null = null;
 
   /* API */
 
@@ -60,15 +65,15 @@ const Model = (() => {
 
   const runWith = ( length: number ): void => {
     clear ();
-    data$ ( buildData ( length ) );
+    $data ( buildData ( length ) );
   };
 
   const add = (): void => {
-    data$ ( [...data$ (), ...buildData ( 1000 )] );
+    $data ( [...$data (), ...buildData ( 1000 )] );
   };
 
   const update = (): void => {
-    const data = data$ ();
+    const data = $data ();
     for ( let i = 0, l = data.length; i < l; i += 10 ) {
       const datum$ = data[i];
       const datum = datum$ ();
@@ -77,53 +82,53 @@ const Model = (() => {
   };
 
   const swapRows = (): void => {
-    const data = data$ ();
+    const data = $data ();
     if ( data.length <= 998 ) return;
     const pos1$ = data[1];
     const pos998$ = data[998];
     const data2 = data.slice ();
     data2[1] = pos998$;
     data2[998] = pos1$;
-    data$ ( data2 );
+    $data ( data2 );
   };
 
   const clear = (): void => {
-    data$ ( [] );
+    $data ( [] );
   };
 
   const remove = ( id: string ): void => {
-    const data = data$ ();
+    const data = $data ();
     const index = data.findIndex ( datum => datum.sample ().id === id );
-    data$ ( [...data.slice ( 0, index ), ...data.slice ( index + 1 )] );
+    $data ( [...data.slice ( 0, index ), ...data.slice ( index + 1 )] );
   };
 
   const select = ( id: string ): void => {
-    if ( selected$ ) {
-      const datum = selected$ ();
+    if ( $selected ) {
+      const datum = $selected ();
       datum.selected ( false );
       datum.className ( '' );
     }
-    const data = data$ ();
+    const data = $data ();
     const datum$ = data.find ( datum => datum.sample ().id === id )!;
     const datum = datum$ ();
     datum.selected ( true );
     datum.className ( 'danger' );
-    selected$ = datum$;
+    $selected = datum$;
   };
 
-  return { data$, selected$, run, runLots, runWith, add, update, swapRows, clear, remove, select };
+  return { $data, $selected, run, runLots, runWith, add, update, swapRows, clear, remove, select };
 
 })();
 
-const Button = ({ id, text, onClick }: { id: string, text: string, onClick: (( event: MouseEvent ) => any) }) => {
-  return (
-    <div class="col-sm-6 smallpad">
-      <button id={id} class="btn btn-primary btn-block" type="button" onClick={onClick}>{text}</button>
-    </div>
-  );
-};
+/* MAIN */
 
-const RowDynamic = ({ id, label, className, onSelect, onRemove }: { id: ObservableMaybe<string>, label: ObservableMaybe<string>, className: ObservableMaybe<string>, onSelect: ObservableMaybe<(( event: MouseEvent ) => any)>, onRemove: ObservableMaybe<(( event: MouseEvent ) => any)> }) => (
+const Button = ({ id, text, onClick }: { id: string, text: string, onClick: (( event: MouseEvent ) => any) }): JSX.Element => (
+  <div class="col-sm-6 smallpad">
+    <button id={id} class="btn btn-primary btn-block" type="button" onClick={onClick}>{text}</button>
+  </div>
+);
+
+const RowDynamic = ({ id, label, className, onSelect, onRemove }: { id: ObservableMaybe<string>, label: ObservableMaybe<string>, className: ObservableMaybe<string>, onSelect: ObservableMaybe<(( event: MouseEvent ) => any)>, onRemove: ObservableMaybe<(( event: MouseEvent ) => any)> }): JSX.Element => (
   <tr class={className}>
     <td class="col-md-1">{id}</td>
     <td class="col-md-4">
@@ -140,8 +145,10 @@ const RowDynamic = ({ id, label, className, onSelect, onRemove }: { id: Observab
 
 const RowTemplate = template ( RowDynamic );
 
-const App = () => {
-  const {data$, run, runLots, add, update, clear, swapRows, select, remove} = Model;
+const App = (): JSX.Element => {
+
+  const {$data, run, runLots, add, update, clear, swapRows, select, remove} = Model;
+
   return (
     <div class="container">
       <div class="jumbotron">
@@ -163,7 +170,7 @@ const App = () => {
       </div>
       <table class="table table-hover table-striped test-data">
         <tbody>
-          <For values={data$}>
+          <For values={$data}>
             {( datum$: IDatum ) => {
               const {id, label, className} = datum$ ();
               const onSelect = () => select ( id );
@@ -178,6 +185,7 @@ const App = () => {
       <span class="preloadicon glyphicon glyphicon-remove" ariaHidden={true}></span>
     </div>
   );
+
 };
 
 render ( <App />, document.getElementById ( 'app' ) );
