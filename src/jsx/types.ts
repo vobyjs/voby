b8a1bd03a27408c214c190c13ca6e71ce83ed79d
high1 <high1@mailfence.com>
2022-05-01 18:38:20 +0200

    Aria comment restored

--- src/jsx/types.ts
+++ src/jsx/types.ts
@@ -478,6 +478,7 @@ declare namespace JSX {
     rel?: FunctionMaybe<string>,
     required?: FunctionMaybe<boolean>,
     reversed?: FunctionMaybe<boolean>,
+    /* WAI-ARIA */
     role?: FunctionMaybe<'alert' | 'alertdialog' | 'application' | 'article' | 'banner' | 'button' | 'cell' | 'checkbox' | 'columnheader' | 'combobox' | 'complementary' | 'contentinfo' | 'definition' | 'dialog' | 'directory' | 'document' | 'feed' | 'figure' | 'form' | 'grid' | 'gridcell' | 'group' | 'heading' | 'img' | 'link' | 'list' | 'listbox' | 'listitem' | 'log' | 'main' | 'marquee' | 'math' | 'menu' | 'menubar' | 'menuitem' | 'menuitemcheckbox' | 'menuitemradio' | 'navigation' | 'none' | 'note' | 'option' | 'presentation' | 'progressbar' | 'radio' | 'radiogroup' | 'region' | 'row' | 'rowgroup' | 'rowheader' | 'scrollbar' | 'search' | 'searchbox' | 'separator' | 'slider' | 'spinbutton' | 'status' | 'switch' | 'tab' | 'table' | 'tablist' | 'tabpanel' | 'term' | 'textbox' | 'timer' | 'toolbar' | 'tooltip' | 'tree' | 'treegrid' | 'treeitem'>,
     rows?: FunctionMaybe<number>,
     rowSpan?: FunctionMaybe<number>,
