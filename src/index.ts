f9222fb58c18f95fd0797b74af5b73c7d264f27e
Fabio Spampinato <spampinabio@gmail.com>
2022-05-02 17:40:53 +0100

    Exporting the JSX runtime functions too from the main entrypoint, for convenience

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import type {FunctionMaybe, Observable, ObservableReadonly, ObservableMaybe, Res
 /* EXPORT */
 
 export * from '~/components';
+export * from '~/jsx/runtime';
 export * from '~/hooks';
 export * from '~/methods';
 export type {FunctionMaybe, Observable, ObservableReadonly, ObservableMaybe, Resource};
