c302ce44f70cce858a3e7879aa47fe65531bf4db
Fabio Spampinato <spampinabio@gmail.com>
2022-05-26 23:15:40 +0100

    Benchmark: importing createElement/Fragment, for ease of bundling purposes

--- demo/benchmark/index.tsx
+++ demo/benchmark/index.tsx
@@ -2,6 +2,7 @@
 /* IMPORT */
 
 import {FunctionMaybe, Observable, ObservableMaybe} from 'voby';
+import {createElement, Fragment} from 'voby';
 import {$, render, template, useSelector, For} from 'voby';
 
 /* TYPES */
