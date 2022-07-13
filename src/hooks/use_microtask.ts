0de2cd292ade8fd7247c7a63be2ca034ce54df2d
Fabio Spampinato <spampinabio@gmail.com>
2022-07-13 17:13:47 +0200

    useMicrotask: ensuring it's not executed if the parent scope if disposed

--- src/hooks/use_microtask.ts
+++ src/hooks/use_microtask.ts
@@ -1,6 +1,7 @@
 
 /* IMPORT */
 
+import useDisposed from '~/hooks/use_disposed';
 import {with as _with} from '~/oby';
 import type {Callback} from '~/types';
 
@@ -8,9 +9,12 @@ import type {Callback} from '~/types';
 
 const useMicrotask = ( fn: Callback ): void => {
 
+  const disposed = useDisposed ();
   const runWithOwner = _with ();
 
   queueMicrotask ( () => {
+
+    if ( disposed () ) return;
 
     runWithOwner ( fn );
 
