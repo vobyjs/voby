b6eba6368c9f886635d02134e87c271676c7df3a
Fabio Spampinato <spampinabio@gmail.com>
2022-08-10 15:53:03 +0100

    Signficantly improved performance when toggling classes, by some ~5x

--- src/utils/classlist.ts
+++ src/utils/classlist.ts
@@ -5,58 +5,53 @@ import {isString} from '~/utils/lang';
 
 /* HELPERS */
 
-const whitespaceRe = /\s+/g;
-const split = ( str: string ): string[] => str.split ( whitespaceRe ).filter ( Boolean );
+const split = ( str: string ): string[] => {
+
+  if ( !str.includes ( ' ' ) ) return [str];
+
+  return str.split ( ' ' ).filter ( Boolean );
+
+};
 
 /* MAIN */
 
-// This function manipulates the "class" attribute directly, whenever possible, because going through the classList API leaks DOMTokenList objects...
+// This function exists to optimize memory usage in some cases, where the classList API won't be touched without sacrificing performance
 
 const classesToggle = ( element: HTMLElement, classes: string, force: null | undefined | boolean ): void => {
 
   const {className} = element;
 
-  if ( !isString ( className ) ) { // "className" is not always a string for SVGs, resorting to classList
+  /* OPTIMIZED PATH */
 
-    split ( classes ).forEach ( cls => {
+  if ( isString ( className ) ) {
 
-      element.classList.toggle ( cls, !!force );
-
-    });
-
-  } else if ( !className ) { // Optimized addition
-
-    if ( force ) {
-
-      element.className = classes;
-
-    }
-
-  } else if ( !force && className === classes ) { // Optimized deletion
-
-    element.className = '';
-
-  } else { // Regular toggling
-
-    const set = new Set ( split ( className ) );
-
-    split ( classes ).forEach ( cls => {
+    if ( !className ) { // Optimized addition
 
       if ( force ) {
 
-        set.add ( cls );
+        element.className = classes;
 
-      } else {
-
-        set.delete ( cls );
+        return;
 
       }
 
-    });
+    } else if ( !force && className === classes ) { // Optimized deletion
 
-    element.className = Array.from ( set ).join ( ' ' );
+      element.className = '';
+
+      return;
+
+    }
 
   }
+
+  /* REGULAR PATH */
+
+  split ( classes ).forEach ( cls => {
+
+    element.classList.toggle ( cls, !!force );
+
+  });
 
 };
 
