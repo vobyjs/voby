f39df1b448e3db7b96c16314c869a3db0c5997ee
Fabio Spampinato <spampinabio@gmail.com>
2023-10-14 16:17:13 +0100

    For: added support for mapping over undefined, for convenience

--- src/components/for.ts
+++ src/components/for.ts
@@ -6,9 +6,9 @@ import type {Child, FunctionMaybe, Indexed, ObservableReadonly} from '~/types';
 
 /* MAIN */
 
-function For <T> ({ values, fallback, pooled, unkeyed, children }: { values: FunctionMaybe<readonly T[]>, fallback?: Child, pooled?: false, unkeyed?: false, children: (( value: T, index: ObservableReadonly<number> ) => Child) }): ObservableReadonly<Child>;
-function For <T> ({ values, fallback, pooled, unkeyed, children }: { values: FunctionMaybe<readonly T[]>, fallback?: Child, pooled?: boolean, unkeyed: true, children: (( value: Indexed<T>, index: ObservableReadonly<number> ) => Child) }): ObservableReadonly<Child>;
-function For <T> ({ values, fallback, pooled, unkeyed, children }: { values: FunctionMaybe<readonly T[]>, fallback?: Child, pooled?: boolean, unkeyed?: boolean, children: (( value: T | Indexed<T>, index: ObservableReadonly<number> ) => Child) }): ObservableReadonly<Child> {
+function For <T> ({ values, fallback, pooled, unkeyed, children }: { values?: FunctionMaybe<readonly T[]>, fallback?: Child, pooled?: false, unkeyed?: false, children: (( value: T, index: ObservableReadonly<number> ) => Child) }): ObservableReadonly<Child>;
+function For <T> ({ values, fallback, pooled, unkeyed, children }: { values?: FunctionMaybe<readonly T[]>, fallback?: Child, pooled?: boolean, unkeyed: true, children: (( value: Indexed<T>, index: ObservableReadonly<number> ) => Child) }): ObservableReadonly<Child>;
+function For <T> ({ values, fallback, pooled, unkeyed, children }: { values?: FunctionMaybe<readonly T[]>, fallback?: Child, pooled?: boolean, unkeyed?: boolean, children: (( value: T | Indexed<T>, index: ObservableReadonly<number> ) => Child) }): ObservableReadonly<Child> {
 
   return _for ( values, children, fallback, { pooled, unkeyed } as any ); //TSC
 
