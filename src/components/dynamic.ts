173c96b0d198cf2cd87c31e7eecb5aa39323ec9d
Fabio Spampinato <spampinabio@gmail.com>
2024-04-02 19:46:43 +0100

    Dynamic: significantly reduced overhead when neither the component nor the props could be reactive

--- src/components/dynamic.ts
+++ src/components/dynamic.ts
@@ -5,17 +5,26 @@ import useMemo from '~/hooks/use_memo';
 import createElement from '~/methods/create_element';
 import resolve from '~/methods/resolve';
 import $$ from '~/methods/SS';
+import {isFunction} from '~/utils/lang';
 import type {Child, Component, FunctionMaybe} from '~/types';
 
 /* MAIN */
 
 const Dynamic = <P = {}> ({ component, props, children }: { component: Component<P>, props?: FunctionMaybe<P | null>, children?: Child }): Child => {
 
-  return useMemo ( () => {
+  if ( isFunction ( component ) || isFunction ( props ) ) {
 
-    return resolve ( createElement<P> ( $$(component, false), $$(props), children ) );
+    return useMemo ( () => {
 
-  });
+      return resolve ( createElement<P> ( $$(component, false), $$(props), children ) );
+
+    });
+
+  } else {
+
+    return createElement<P> ( component, props, children );
+
+  }
 
 };
 
